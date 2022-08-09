cd7968bcb640679be309a86abe5ab8a4b5a1ea7e
Gleb <gapodgorsky@yandex.ru>
2022-08-10 00:32:44 +0300

    Add Three-digit number

---
+++ Example005_Three-digit_number/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N >= 100 && N <= 999)
+{
+    int lastNumber = N % 10;
+    Console.WriteLine("А вот и она, цифра твоя = " + lastNumber);
+}
+else
+{
+    Console.WriteLine("Неее, это не трехзначное число, запусти по новой!");
+}
