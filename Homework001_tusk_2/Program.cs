8b7e79dacae162f48fdaed6ba7314103abb66901
Gleb <gapodgorsky@yandex.ru>
2022-08-10 00:52:08 +0300

    Add Homework for tusk 2

---
+++ Homework001_tusk_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Add number a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Add number b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    int BiggestNumber = a;
+    Console.WriteLine("a - is a winner!");
+    Console.Write(BiggestNumber);
+}
+else
+{
+    int BiggestNumber = b;
+    Console.WriteLine("b - is a winner!");
+    Console.Write(BiggestNumber);
+}
