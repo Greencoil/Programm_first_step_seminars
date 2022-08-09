ebca695b93d5c42cedc0019fd2e400a37f8cbec2
Gleb <gapodgorsky@yandex.ru>
2022-08-09 23:33:37 +0300

    Add week = number

---
+++ Example003_week/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Add number 1- 7 please");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+case 1:
+{
+    Console.WriteLine("Monday");
+    break;
+}    
+case 2: 
+{
+    Console.WriteLine("Tuesday");
+    break;
+}
+case 3:
+{
+    Console.WriteLine("Wednesday");
+    break;
+}
+case 4:
+{
+    Console.WriteLine("Thursday");
+    break;
+}
+case 5:
+{
+    Console.WriteLine("Friday");
+    break;
+}
+case 6:
+{
+    Console.WriteLine("Saturday");
+    break;
+}
+case 7:
+{
+    Console.WriteLine("Sunday");
+    break;
+}
+}
+
