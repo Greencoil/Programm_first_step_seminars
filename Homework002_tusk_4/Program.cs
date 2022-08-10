40db9c557135f6b784947c428341353f3a3918ef
Gleb <gapodgorsky@yandex.ru>
2022-08-10 21:09:14 +0300

    add tusk_4

---
+++ Homework002_tusk_4/Program.cs
@@ -0,0 +1,15 @@
+﻿int max = 0;
+Console.WriteLine("Add number a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Add number b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Add number c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+
+Console.Write("The biggest number is = " + max);
