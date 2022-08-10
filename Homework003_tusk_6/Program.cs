00bef1aaeb1edce7d092692881f3bed47b1d10d2
Gleb <gapodgorsky@yandex.ru>
2022-08-10 22:08:24 +0300

    add tusk_6

---
+++ Homework003_tusk_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Write new number ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.Write("YES, this number is even");
+}
+else
+{
+    Console.Write("NO, this number is odd");
+}
