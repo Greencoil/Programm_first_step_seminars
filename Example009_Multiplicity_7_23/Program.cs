2086c82ba300c731c1714e241a42678241f6f6e8
Gleb <gapodgorsky@yandex.ru>
2022-08-14 23:50:19 +0300

    Add Example009

---
+++ Example009_Multiplicity_7_23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Add new number a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 & a % 23 == 0)
+{
+    Console.WriteLine("Multiple 7 & 23");
+}
+else
+{
+    Console.WriteLine("Not multiple 7 & 23");
+}
