cf3c60d3282ae1f4f880b290f1d823eb98468b88
Gleb <gapodgorsky@yandex.ru>
2022-08-14 22:57:30 +0300

    Add Example006

---
+++ Example006_max_number/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = new Random().Next(10, 100);
+
+Console.WriteLine("Rndom number from 10 to 99 " + n);
+
+int FirstDigit = n / 10;
+int SecondDigit = n % 10;
+
+if (FirstDigit > SecondDigit)
+{
+    Console.WriteLine("Maximum number is " + FirstDigit);
+}
+else
+{
+    Console.WriteLine("Maximum number is " + SecondDigit);
+}
+
