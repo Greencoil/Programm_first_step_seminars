dbf48405b6b40b07c03fb174fd7e2567c6129241
Gleb <gapodgorsky@yandex.ru>
2022-08-15 00:03:13 +0300

    Add Example010

---
+++ Example010_Are_number(b)_is square_number(a)/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Add new number a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Add new number b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if ((a == b * b) ^ (b == a * a))
+{
+    Console.WriteLine("Yes, this is a square");
+}
+else
+{
+    Console.WriteLine("No, this is not a square");
+}
