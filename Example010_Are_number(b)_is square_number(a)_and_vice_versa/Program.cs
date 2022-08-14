fcb448f019811b283bfa4d7556110637faf17364
Gleb <gapodgorsky@yandex.ru>
2022-08-15 00:06:51 +0300

    Add Example010 remark2

--- Example010_Are_number(b)_is square_number(a)_and_vice_versa/Program.cs
+++ Example010_Are_number(b)_is square_number(a)_and_vice_versa/Program.cs
@@ -4,6 +4,7 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Add new number b = ");
 int b = Convert.ToInt32(Console.ReadLine());
 
+//if ((a == b * b) || (b == a * a))
 if ((a == b * b) ^ (b == a * a))
 {
     Console.WriteLine("Yes, this is a square");
