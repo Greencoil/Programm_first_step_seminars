e76961d23a6c026864bad5f38a5a343552bb8bbd
Gleb <gapodgorsky@yandex.ru>
2022-08-14 23:37:29 +0300

    Add Example008

---
+++ Exmple008_Multiplicity/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Add new number a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Add new number b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a / b == 0)
+{
+    Console.WriteLine("Multiple");
+}
+else
+{
+    Console.WriteLine("Remains " + a / b );
+}
