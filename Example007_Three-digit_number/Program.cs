89f59acd884da15d2a5c48766484d7d3a8c6f7ce
Gleb <gapodgorsky@yandex.ru>
2022-08-14 23:08:10 +0300

    Add Example007

---
+++ Example007_Three-digit_number/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = new Random().Next(100, 1000);
+
+Console.WriteLine("Three-digit number is " + n);
+
+int FirstDigit = n / 100;
+int ThirdDigit = n % 10;
+
+int b = 10 * FirstDigit + ThirdDigit;
+
+Console.WriteLine("Two-digit number is " + b);
