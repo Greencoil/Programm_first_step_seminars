560daddaeeea90c2eb20eaaef0935f55b78c153b
Gleb <gapodgorsky@yandex.ru>
2022-08-16 02:42:55 +0300

    Add Homework006

---
+++ Homework006_tusk_13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Add new number ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (a > 999)
+{
+   a = a / 10;
+}
+if (a > 99)
+{
+   int b = a % 10;
+   Console.WriteLine("There is three-digit number " + b);
+} 
+else
+{
+   Console.WriteLine("It is not three-digit number");
+}
