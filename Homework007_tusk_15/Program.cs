5abb9fa63aaa92b2679a79b96e1580723bdb3df3
Gleb <gapodgorsky@yandex.ru>
2022-08-16 02:51:17 +0300

    Add Homework007

---
+++ Homework007_tusk_15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter the number of the day of the week ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 6 ^ a == 7)
+{
+    Console.WriteLine("Yes, it's a day off");
+}
+else
+{
+    Console.WriteLine("No, it is work day");
+}
