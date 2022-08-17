79da80a2882c8be22d8d403cc205789cf84649f0
Gleb <gapodgorsky@yandex.ru>
2022-08-18 01:39:49 +0300

    add example014

---
+++ Example014_square_from_N_till_N/Program.cs
@@ -0,0 +1,30 @@
+﻿// Console.WriteLine("Введите целое чило ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while (i <= Math.Abs(a))
+// {
+//     if (i == a)
+//     {
+//         Console.Write(i * i);
+//     }
+//     else
+//     {
+//         Console.Write($"{i * i}, ");
+//     }
+    
+//     i ++;
+// }
+
+Console.WriteLine("Введите целое чило ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= Math.Abs(a))
+{
+    Console.Write(i * i);
+    if (i < Math.Abs(a))
+    {
+        Console.Write(", ");
+    }
+        
+    i ++;
+}
