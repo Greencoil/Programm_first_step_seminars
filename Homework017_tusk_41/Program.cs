c4d2f16e4a0542f30fd1dbd8facd481612871b7c
Gleb <gapodgorsky@yandex.ru>
2022-09-02 23:31:47 +0300

    add homework for seminar 6

---
+++ Homework017_tusk_41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Enter numbers separated by a space: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Positive numbers: " + count);
---
+++ Homework018_tusk_43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Enter a1: ");
+double a1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter b1: ");
+double b1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter a2: ");
+double a2 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter b2: ");
+double b2 = int.Parse(Console.ReadLine());
+
+Console.Write($"Points: a1 = {a1}, b1 = {b1}, a2 = {a2}, b2 = {b2}");
+
+double x = (b2 - b1) / (a1 - a2);
+double y = a1 * x + b1;
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.Write($"Coordinates: x = {x}, y = {y}");
